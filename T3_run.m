a2273a2603d001497e2eb9dd41cc84fc8b63af61
ElChololo <aacevedod@fen.uchile.cl>
2022-06-24 14:03:26 -0400

    Update T3_run.m
    P6

--- T3_run.m
+++ T3_run.m
@@ -1,10 +1,11 @@
-%% Preparación, Preguntas 1 2 3
+%% PreparaciÃ³n, Preguntas 1 2 3
 obj = T3_obj('docvis.xlsx');
 
-%% Pregunta 3
-beta_est = obj.MetNum("NR",100,[0 0 0 0 0],obj.regresores,obj.Y);
 %% Pregunta 4
+beta_est = obj.MetNum("NR",100,[0 0 0 0 0],obj.regresores,obj.Y);
+%% Pregunta 5
 beta_est = obj.MetNum("BHHH",1000,[0 0 0 0 0],obj.regresores,obj.Y);
 
-%% Pregunta 5
-beta_est = obj.NewtownRaph("BHHH",100,[0],obj.regresores,obj.Y);
+%% Pregunta 6
+beta_est = obj.NR_R(100,obj.Y,0);
+
